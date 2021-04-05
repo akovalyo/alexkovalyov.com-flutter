23bda8c5e83132bc3fd37e73f8902a27cab2b64a
Alexander Kovalyov <al.kovalyov@gmail.com>
2021-04-04 21:17:31 -0500

    Replace \t in Text widget

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hovering/hovering.dart';
 
-import 'package:mysite/helpers.dart';
-
 class AkButton extends StatelessWidget {
   final VoidCallback onPressed;
   final Widget child;
