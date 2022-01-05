3fd9ea14b5bd38368ba0c409114b572b6d94825e
Alexander Kovalyov <al.kovalyov@gmail.com>
2022-01-05 17:43:37 -0600

    Move PostElementContainer into a separate file

---
+++ lib/widgets/post_element_container.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class PostElementContainer extends StatelessWidget {
+  final Widget child;
+  final double vertPadding;
+  final double horPadding;
+  final Color background;
+  final Alignment alignment;
+
+  PostElementContainer({
+    required this.child,
+    this.vertPadding = 0,
+    this.horPadding = 0,
+    this.background = Colors.white,
+    this.alignment = Alignment.centerLeft,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: alignment,
+      padding: EdgeInsets.symmetric(
+        vertical: vertPadding,
+        horizontal: horPadding,
+      ),
+      child: child,
+      color: background,
+    );
+  }
+}
