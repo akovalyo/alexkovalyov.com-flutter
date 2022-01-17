5a5740fb17571efafa9188ffac91712ba84b03d0
Alexander Kovalyov <al.kovalyov@gmail.com>
2022-01-17 17:25:04 -0600

    New fonts

--- lib/consts/consts.dart
+++ lib/consts/consts.dart
@@ -4,11 +4,8 @@ const appBarHeight = 70.0;
 const footerHeight = 120.0;
 const appBarPadding = 40.0;
 const tileLargeWidth = 380.0;
-
 const tileLargeHeight = 160.0;
-
 const tileSmallWidth = 400.0;
-
 const tileSmallHeight = 180.0;
 
 const scrollBarThickness = 8.0;
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -24,49 +24,49 @@ class AppThemes {
       hoverColor: Color(0x00000000),
       textTheme: ThemeData.light().textTheme.copyWith(
             caption: ThemeData.light().textTheme.caption!.copyWith(
-                  fontFamily: 'DavidLibre',
-                  fontWeight: FontWeight.w100,
+                  fontFamily: 'Oswald',
+                  fontWeight: FontWeight.w300,
                   fontSize: 20,
                   height: 1.5,
                   color: akAccentL,
                   decoration: TextDecoration.underline,
                 ),
             bodyText1: ThemeData.light().textTheme.bodyText1!.copyWith(
-                  fontFamily: 'DavidLibre',
-                  fontWeight: FontWeight.w100,
+                  fontFamily: 'Oswald',
+                  fontWeight: FontWeight.w300,
                   fontSize: 18,
                   height: 1.5,
                   color: Colors.white,
                 ),
             bodyText2: ThemeData.light().textTheme.bodyText2!.copyWith(
-                  fontFamily: 'DavidLibre',
-                  fontWeight: FontWeight.w100,
+                  fontFamily: 'Oswald',
+                  fontWeight: FontWeight.w300,
                   fontSize: 18,
                   height: 1.5,
                 ),
             headline3: ThemeData.light().textTheme.headline3!.copyWith(
-                  fontFamily: 'DavidLibre',
+                  fontFamily: 'Oswald',
                   color: akHeadlineL,
-                  fontWeight: FontWeight.bold,
+                  fontWeight: FontWeight.w500,
                   fontSize: 40,
                 ),
             headline4: ThemeData.light().textTheme.headline4!.copyWith(
-                  fontFamily: 'DavidLibre',
+                  fontFamily: 'Oswald',
                   color: akHeadlineL,
-                  fontWeight: FontWeight.bold,
+                  fontWeight: FontWeight.w500,
                 ),
             headline5: ThemeData.light().textTheme.headline5!.copyWith(
-                  fontFamily: 'DavidLibre',
+                  fontFamily: 'Oswald',
                   color: akHeadlineL,
                   fontSize: 28,
                 ),
             headline6: ThemeData.light().textTheme.headline6!.copyWith(
-                  fontFamily: 'DavidLibre',
+                  fontFamily: 'Oswald',
                   color: akHeadlineL,
                   fontSize: 24,
                 ),
             button: ThemeData.light().textTheme.button!.copyWith(
-                  fontFamily: 'DavidLibre',
+                  fontFamily: 'Oswald',
                   color: Colors.white,
                 ),
           ),
@@ -95,50 +95,50 @@ class AppThemes {
       hoverColor: Color(0x00000000),
       textTheme: ThemeData.dark().textTheme.copyWith(
             caption: ThemeData.dark().textTheme.caption!.copyWith(
-                  fontFamily: 'DavidLibre',
-                  fontWeight: FontWeight.w100,
+                  fontFamily: 'Oswald',
+                  fontWeight: FontWeight.w200,
                   fontSize: 20,
                   height: 1.5,
                   color: akAccentD,
                   decoration: TextDecoration.underline,
                 ),
             bodyText1: ThemeData.dark().textTheme.bodyText1!.copyWith(
-                  fontFamily: 'DavidLibre',
-                  fontWeight: FontWeight.w100,
+                  fontFamily: 'Oswald',
+                  fontWeight: FontWeight.w200,
                   fontSize: 20,
                   height: 1.5,
                   color: akHeadlineD,
                 ),
             bodyText2: ThemeData.dark().textTheme.bodyText2!.copyWith(
-                  fontFamily: 'DavidLibre',
-                  fontWeight: FontWeight.w100,
+                  fontFamily: 'Oswald',
+                  fontWeight: FontWeight.w200,
                   fontSize: 20,
                   height: 1.5,
                   color: akHeadlineD,
                 ),
             headline3: ThemeData.dark().textTheme.headline3!.copyWith(
-                  fontFamily: 'DavidLibre',
+                  fontFamily: 'Oswald',
                   color: akHeadlineL,
-                  fontWeight: FontWeight.bold,
+                  fontWeight: FontWeight.w500,
                   fontSize: 40,
                 ),
             headline4: ThemeData.dark().textTheme.headline4!.copyWith(
-                  fontFamily: 'DavidLibre',
+                  fontFamily: 'Oswald',
                   color: akHeadlineD,
-                  fontWeight: FontWeight.bold,
+                  fontWeight: FontWeight.w500,
                 ),
             headline5: ThemeData.dark().textTheme.headline5!.copyWith(
-                  fontFamily: 'DavidLibre',
+                  fontFamily: 'Oswald',
                   color: akHeadlineD,
                   fontSize: 28,
                 ),
             headline6: ThemeData.dark().textTheme.headline6!.copyWith(
-                  fontFamily: 'DavidLibre',
+                  fontFamily: 'Oswald',
                   color: akHeadlineD,
                   fontSize: 24,
                 ),
             button: ThemeData.dark().textTheme.button!.copyWith(
-                  fontFamily: 'DavidLibre',
+                  fontFamily: 'Oswald',
                   color: Colors.white,
                 ),
           ),
@@ -171,23 +171,23 @@ class AppThemes {
       hoverColor: Color(0x00000000),
       textTheme: ThemeData.light().textTheme.copyWith(
             caption: ThemeData.light().textTheme.caption!.copyWith(
-                  fontFamily: 'DavidLibre',
-                  fontWeight: FontWeight.w100,
+                  fontFamily: 'Oswald',
+                  fontWeight: FontWeight.w200,
                   fontSize: 20,
                   height: 1.5,
                   color: cpBlue,
                   decoration: TextDecoration.underline,
                 ),
             bodyText1: ThemeData.light().textTheme.bodyText1!.copyWith(
-                  fontFamily: 'DavidLibre',
-                  fontWeight: FontWeight.w100,
+                  fontFamily: 'Oswald',
+                  fontWeight: FontWeight.w200,
                   fontSize: 20,
                   height: 1.5,
                   color: cp2077,
                 ),
             bodyText2: ThemeData.light().textTheme.bodyText2!.copyWith(
-                  fontFamily: 'DavidLibre',
-                  fontWeight: FontWeight.w100,
+                  fontFamily: 'Oswald',
+                  fontWeight: FontWeight.w200,
                   fontSize: 20,
                   height: 1.5,
                 ),
@@ -211,7 +211,7 @@ class AppThemes {
                   fontSize: 24,
                 ),
             button: ThemeData.light().textTheme.button!.copyWith(
-                  fontFamily: 'DavidLibre',
+                  fontFamily: 'Oswald',
                   color: Colors.white,
                 ),
           ),
@@ -227,219 +227,3 @@ class AppThemes {
     );
   }
 }
-
-// ThemeData akTheme(Brightness brightness, String? theme) {
-//   if (theme == '2077') {
-//     return ThemeData.light().copyWith(
-//       brightness: brightness,
-//       primaryColor: akBlackL,
-//       secondaryHeaderColor: cp2077,
-//       primaryColorDark: akBlackLAcc,
-//       backgroundColor: cp2077,
-//       errorColor: akAlertL,
-//       splashColor: Color(0x00000000),
-//       highlightColor: Color(0x00000000),
-//       hoverColor: Color(0x00000000),
-//       textTheme: ThemeData.light().textTheme.copyWith(
-//             caption: ThemeData.light().textTheme.caption!.copyWith(
-//                   fontFamily: 'DavidLibre',
-//                   fontWeight: FontWeight.w100,
-//                   fontSize: 20,
-//                   height: 1.5,
-//                   color: cpBlue,
-//                   decoration: TextDecoration.underline,
-//                 ),
-//             bodyText1: ThemeData.light().textTheme.bodyText1!.copyWith(
-//                   fontFamily: 'DavidLibre',
-//                   fontWeight: FontWeight.w100,
-//                   fontSize: 20,
-//                   height: 1.5,
-//                   color: cp2077,
-//                 ),
-//             bodyText2: ThemeData.light().textTheme.bodyText2!.copyWith(
-//                   fontFamily: 'DavidLibre',
-//                   fontWeight: FontWeight.w100,
-//                   fontSize: 20,
-//                   height: 1.5,
-//                 ),
-//             headline3: ThemeData.light().textTheme.headline3!.copyWith(
-//                   fontFamily: 'Cyberpunk',
-//                   color: Colors.black,
-//                   fontSize: 40,
-//                 ),
-//             headline4: ThemeData.light().textTheme.headline4!.copyWith(
-//                   fontFamily: 'Cyberpunk',
-//                   color: cpBlue,
-//                 ),
-//             headline5: ThemeData.light().textTheme.headline5!.copyWith(
-//                   fontFamily: 'Cyberpunk',
-//                   color: cpBlue,
-//                   fontSize: 28,
-//                 ),
-//             headline6: ThemeData.light().textTheme.headline6!.copyWith(
-//                   fontFamily: 'Cyberpunk',
-//                   color: cpBlue,
-//                   fontSize: 24,
-//                 ),
-//             button: ThemeData.light().textTheme.button!.copyWith(
-//                   fontFamily: 'DavidLibre',
-//                   color: Colors.white,
-//                 ),
-//           ),
-//       canvasColor: akBlackL,
-//       iconTheme: _akIconTheme(ThemeData.light().iconTheme, cpBlue),
-//       buttonTheme: const ButtonThemeData(
-//         textTheme: ButtonTextTheme.primary,
-//         buttonColor: Colors.black,
-//       ),
-//       colorScheme: ThemeData.light().colorScheme.copyWith(
-//             secondary: cpBlue,
-//           ),
-//     );
-//   }
-
-//   return brightness == Brightness.light
-//       ? ThemeData.light().copyWith(
-//           brightness: brightness,
-//           primaryColor: akBlackL,
-//           primaryColorDark: akBlackLAcc,
-//           secondaryHeaderColor: Colors.white,
-//           backgroundColor: Colors.white,
-//           errorColor: akAlertL,
-//           splashColor: Color(0x00000000),
-//           highlightColor: Color(0x00000000),
-//           hoverColor: Color(0x00000000),
-//           textTheme: ThemeData.light().textTheme.copyWith(
-//                 caption: ThemeData.light().textTheme.caption!.copyWith(
-//                       fontFamily: 'DavidLibre',
-//                       fontWeight: FontWeight.w100,
-//                       fontSize: 20,
-//                       height: 1.5,
-//                       color: akAccentL,
-//                       decoration: TextDecoration.underline,
-//                     ),
-//                 bodyText1: ThemeData.light().textTheme.bodyText1!.copyWith(
-//                       fontFamily: 'DavidLibre',
-//                       fontWeight: FontWeight.w100,
-//                       fontSize: 20,
-//                       height: 1.5,
-//                       color: Colors.white,
-//                     ),
-//                 bodyText2: ThemeData.light().textTheme.bodyText2!.copyWith(
-//                       fontFamily: 'DavidLibre',
-//                       fontWeight: FontWeight.w100,
-//                       fontSize: 20,
-//                       height: 1.5,
-//                     ),
-//                 headline3: ThemeData.light().textTheme.headline3!.copyWith(
-//                       fontFamily: 'DavidLibre',
-//                       color: akHeadlineL,
-//                       fontWeight: FontWeight.bold,
-//                       fontSize: 40,
-//                     ),
-//                 headline4: ThemeData.light().textTheme.headline4!.copyWith(
-//                       fontFamily: 'DavidLibre',
-//                       color: akHeadlineL,
-//                       fontWeight: FontWeight.bold,
-//                     ),
-//                 headline5: ThemeData.light().textTheme.headline5!.copyWith(
-//                       fontFamily: 'DavidLibre',
-//                       color: akHeadlineL,
-//                       fontSize: 28,
-//                     ),
-//                 headline6: ThemeData.light().textTheme.headline6!.copyWith(
-//                       fontFamily: 'DavidLibre',
-//                       color: akHeadlineL,
-//                       fontSize: 24,
-//                     ),
-//                 button: ThemeData.light().textTheme.button!.copyWith(
-//                       fontFamily: 'DavidLibre',
-//                       color: Colors.white,
-//                     ),
-//               ),
-//           canvasColor: akBlackL,
-//           iconTheme: _akIconTheme(ThemeData.light().iconTheme, Colors.white),
-//           buttonTheme: ButtonThemeData(
-//             textTheme: ButtonTextTheme.primary,
-//             buttonColor: akBlackL,
-//           ),
-//           colorScheme: ThemeData.light().colorScheme.copyWith(
-//                 secondary: akAccentL,
-//               ),
-//         )
-//       : ThemeData.dark().copyWith(
-//           brightness: brightness,
-//           primaryColor: akBlackD,
-//           primaryColorDark: akBlackDAcc,
-//           backgroundColor: akBackD,
-//           secondaryHeaderColor: akHeadlineD,
-//           errorColor: akAlertD,
-//           splashColor: Color(0x00000000),
-//           highlightColor: Color(0x00000000),
-//           hoverColor: Color(0x00000000),
-//           textTheme: ThemeData.dark().textTheme.copyWith(
-//                 caption: ThemeData.dark().textTheme.caption!.copyWith(
-//                       fontFamily: 'DavidLibre',
-//                       fontWeight: FontWeight.w100,
-//                       fontSize: 20,
-//                       height: 1.5,
-//                       color: akAccentD,
-//                       decoration: TextDecoration.underline,
-//                     ),
-//                 bodyText1: ThemeData.dark().textTheme.bodyText1!.copyWith(
-//                       fontFamily: 'DavidLibre',
-//                       fontWeight: FontWeight.w100,
-//                       fontSize: 20,
-//                       height: 1.5,
-//                       color: akHeadlineD,
-//                     ),
-//                 bodyText2: ThemeData.dark().textTheme.bodyText2!.copyWith(
-//                       fontFamily: 'DavidLibre',
-//                       fontWeight: FontWeight.w100,
-//                       fontSize: 20,
-//                       height: 1.5,
-//                       color: akHeadlineD,
-//                     ),
-//                 headline3: ThemeData.dark().textTheme.headline3!.copyWith(
-//                       fontFamily: 'DavidLibre',
-//                       color: akHeadlineL,
-//                       fontWeight: FontWeight.bold,
-//                       fontSize: 40,
-//                     ),
-//                 headline4: ThemeData.dark().textTheme.headline4!.copyWith(
-//                       fontFamily: 'DavidLibre',
-//                       color: akHeadlineD,
-//                       fontWeight: FontWeight.bold,
-//                     ),
-//                 headline5: ThemeData.dark().textTheme.headline5!.copyWith(
-//                       fontFamily: 'DavidLibre',
-//                       color: akHeadlineD,
-//                       fontSize: 28,
-//                     ),
-//                 headline6: ThemeData.dark().textTheme.headline6!.copyWith(
-//                       fontFamily: 'DavidLibre',
-//                       color: akHeadlineD,
-//                       fontSize: 24,
-//                     ),
-//                 button: ThemeData.dark().textTheme.button!.copyWith(
-//                       fontFamily: 'DavidLibre',
-//                       color: Colors.white,
-//                     ),
-//               ),
-//           canvasColor: akBlackL,
-//           iconTheme: _akIconTheme(ThemeData.dark().iconTheme, Colors.white),
-//           buttonTheme: const ButtonThemeData(
-//             highlightColor: Color(0x00000000),
-//             splashColor: Color(0x00000000),
-//             hoverColor: Color(0x00000000),
-//             buttonColor: Color(0x00000000),
-//             focusColor: Color(0x00000000),
-//             textTheme: ButtonTextTheme.primary,
-//           ),
-//           colorScheme: ThemeData.light().colorScheme.copyWith(
-//                 secondary: akAccentD,
-//               ),
-//         );
-// }
-
-
