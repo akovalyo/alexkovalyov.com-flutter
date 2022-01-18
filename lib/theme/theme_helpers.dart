7e1549f37c9f9eecf83971411528ed14c504266c
Alexander Kovalyov <al.kovalyov@gmail.com>
2022-01-17 18:36:49 -0600

    Add theme helpers

---
+++ lib/theme/theme_helpers.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Set<MaterialState> interactiveStates = <MaterialState>{
+  MaterialState.pressed,
+  MaterialState.hovered,
+  MaterialState.focused,
+};
