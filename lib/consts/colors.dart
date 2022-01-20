58eb383d656da1b6f730d7f2f8406e79283f17f6
Alexander Kovalyov <al.kovalyov@gmail.com>
2022-01-20 17:08:34 -0600

    Add ThemeColors

--- lib/consts/colors.dart
+++ lib/consts/colors.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-const akBlackL = Color(0xFF3F3F3F);
-const akBlackLAcc = Color(0xFF353535);
+// const akBlackL = Color(0xFF3F3F3F);
+// const akBlackLAcc = Color(0xFF353535);
 
-const akBlackD = Color(0xFF202020);
-const akBlackDAcc = Color(0xFF2B2B2B);
+// const akBlackD = Color(0xFF202020);
+// const akBlackDAcc = Color(0xFF2B2B2B);
 
-const akBackD = Color(0xFF747474);
+// const akBackD = Color(0xFF747474);
 
-const akAccentL = Color(0xFFA43C28);
-const akAccentD = Color(0xFF9F483D);
+// const akAccentL = Color(0xFFA43C28);
+// const akAccentD = Color(0xFF9F483D);
 
 const akGrey = Color(0xFF707070);
 
@@ -17,7 +17,7 @@ const akAlertL = Color(0xFFA43C28);
 const akAlertD = Color(0xFFA43C28);
 
 const akHeadlineL = Color(0xFF2E2E2E);
-const akHeadlineD = Color(0xFFBDB3B3);
+// const akHeadlineD = Color(0xFFBDB3B3);
 
 const cp2077 = Color(0xFFf3e500);
 const cpBlue = Color(0xFF70C4F4);
