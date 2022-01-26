d6e394f8082da6661d1001f6ddd049d61fe08999
Alexander Kovalyov <al.kovalyov@gmail.com>
2022-01-25 21:47:16 -0600

    Add NftGalleryOrderBy helper

--- lib/helpers/nft_helper.dart
+++ lib/helpers/nft_helper.dart
@@ -34,3 +34,32 @@ class Rarity {
     legendary,
   ];
 }
+
+class NftGalleryOrderBy {
+  static String dateNewest = 'Date (newest)';
+  static String dateOldest = 'Date (oldest)';
+  static String mintedNewest = 'Minted (newest)';
+  static String mintedOldest = 'Minted (oldest)';
+  static String name = 'Name';
+  static String collection = 'Collection';
+  static String tokenIdHighest = 'Token ID (highest)';
+  static String tokenIdLowest = 'Token ID (lowest)';
+  static String createdBy = 'Created by';
+  static String tokenStandard = 'Token standard';
+  static String blockchain = 'Blockchain';
+  static String rarityRank = 'Rarity rank';
+
+  static List<String> all = [
+    dateNewest,
+    dateOldest,
+    mintedNewest,
+    mintedOldest,
+    name,
+    collection,
+    tokenIdHighest,
+    tokenIdLowest,
+    createdBy,
+    blockchain,
+    rarityRank,
+  ];
+}
